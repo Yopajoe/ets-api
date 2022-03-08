574cbc02bc0054a7f8e68e23518128e05001ee80
Yopajoe <51920923+Yopajoe@users.noreply.github.com>
2022-03-08 20:28:25 +0100

    Update shema.php

--- config/shema.php
+++ config/shema.php
@@ -5,31 +5,7 @@ return [
     "index" => [
         "msg" => "welcome",
         "links" => [
-            "/shema/rezervacija",
-            "/shema/musterija",
-            "/shema/gost",
-            "/shema/uplata",
-            "/shema/rezervacija_sobe",
-            "/shema/tip_dodatka",
-            "/shema/soba"
+            
         ]
-    ],
-
-    "rezervacija_sobe" => [
-        
-        "link" => "/rezervacija_sobe",
-
-        "params" => [
-            "RezervacijaID : integer",
-            "SobaID : integer",
-            "GostID : integer"
-        ],
-        "exemple" => [
-            "RezervacijaID" => 3,
-            "SobaID" => 8,
-            "GostID" => 4 
-        ]
-    
     ]
-
 ];
